9f417448ecebdb0e9fdcc2a691e3b0634a2d1588
egorov-g-a <egorov-g-a@users.noreply.github.com>
2025-01-09 16:14:12 +0400

    Work with drawer

--- draw/gen-images.sh
+++ draw/gen-images.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+echo "Parsing ZMK keymap..."
 # keymap -c keymap_config.yaml parse -c 12 -z ../config/corne.keymap >corne.yaml
 keymap -c keymap_config.yaml parse -c 12 -l Base Diktor NAV FN NUM Sys Mouse -z ../config/corne.keymap >corne.yaml
 # keymap -c config.yaml parse -c 12 -z ../config/corne.keymap >corne.yaml
-keymap -c keymap_config.yaml draw corne.yaml -k "crkbd/rev1" -l "LAYOUT_split_3x6_3" --keys-only >corne.svg
-keymap -c keymap_config.yaml draw corne.yaml -k "crkbd/rev1" -l "LAYOUT_split_3x6_3" -s Base --combos-only >combos.svg
+echo -e "\nDrawing keymap..."
+keymap -c keymap_config.yaml draw -k "crkbd/rev1" -l "LAYOUT_split_3x6_3" --keys-only corne.yaml >corne.svg
+keymap -c keymap_config.yaml draw -k "crkbd/rev1" -l "LAYOUT_split_3x6_3" -s Base --combos-only corne.yaml >combos.svg
