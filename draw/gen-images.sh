7bbeea4ff1c2a87818aea9654a5fca0769ef42ab
Egorov Gennady <egorov_g_a@mail.ru>
2025-01-24 16:01:22 +0400

    Rus keymaps in Diktor

--- draw/gen-images.sh
+++ draw/gen-images.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
+# required yq version v4.x
+yq eval-all '. as $item ireduce ({}; . * $item )' keymap_config.yaml rus_keycodes.yaml >keymap_config_rus.yaml
 echo "Parsing ZMK keymap..."
-keymap -c keymap_config.yaml parse -c 12 -l Base Diktor NAV FN NUM Sys Mouse -z ../config/corne.keymap >corne.yaml
+keymap --config keymap_config.yaml parse --columns 12 --layer-names Base Diktor NAV FN NUM Sys Mouse --zmk-keymap ../config/corne.keymap >corne_orig.yaml
+keymap --config keymap_config_rus.yaml parse --columns 12 --layer-names Base Diktor NAV FN NUM Sys Mouse --zmk-keymap ../config/corne.keymap >corne_rus.yaml
+yq eval-all 'select(fileIndex==0).layers.Diktor = select(fileIndex==1).layers.Diktor | select(fileIndex==0)' corne_orig.yaml corne_rus.yaml >corne.yaml
 echo -e "\nDrawing keymap..."
-keymap -c keymap_config.yaml draw -k "crkbd/rev1" -l "LAYOUT_split_3x6_3" --keys-only corne.yaml >corne.svg
-keymap -c keymap_config.yaml draw -k "crkbd/rev1" -l "LAYOUT_split_3x6_3" -s Base NAV --combos-only corne.yaml >combos.svg
+keymap --config keymap_config.yaml draw --qmk-keyboard "crkbd/rev1" --layout-name "LAYOUT_split_3x6_3" --keys-only corne.yaml >corne.svg
+keymap --config keymap_config_rus.yaml draw --qmk-keyboard "crkbd/rev1" --layout-name "LAYOUT_split_3x6_3" --select-layers Diktor --keys-only corne.yaml >corne_rus.svg
+keymap --config keymap_config.yaml draw --qmk-keyboard "crkbd/rev1" --layout-name "LAYOUT_split_3x6_3" --select-layers Base NAV --combos-only corne.yaml >combos.svg
