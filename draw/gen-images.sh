c759bb0677fe938df4035b9420973f79131cfaf5
Egorov Gennady <egorov_g_a@mail.ru>
2025-02-04 15:59:43 +0400

    Added symbol layer

--- draw/gen-images.sh
+++ draw/gen-images.sh
@@ -2,8 +2,8 @@
 # required yq version v4.x
 yq eval-all '. as $item ireduce ({}; . * $item )' keymap_config.yaml rus_keycodes.yaml >keymap_config_rus.yaml
 echo "Parsing ZMK keymap..."
-keymap --config keymap_config.yaml parse --columns 12 --layer-names Base Diktor NAV FN NUM Sys Mouse --zmk-keymap ../config/corne.keymap >corne_orig.yaml
-keymap --config keymap_config_rus.yaml parse --columns 12 --layer-names Base Diktor NAV FN NUM Sys Mouse --zmk-keymap ../config/corne.keymap >corne_rus.yaml
+keymap --config keymap_config.yaml parse --columns 12 --layer-names Base Diktor NAV FN NUM SYM Sys Mouse --zmk-keymap ../config/corne.keymap >corne_orig.yaml
+keymap --config keymap_config_rus.yaml parse --columns 12 --layer-names Base Diktor NAV FN NUM SYM Sys Mouse --zmk-keymap ../config/corne.keymap >corne_rus.yaml
 yq eval-all 'select(fileIndex==0).layers.Diktor = select(fileIndex==1).layers.Diktor | select(fileIndex==0)' corne_orig.yaml corne_rus.yaml >corne.yaml
 echo -e "\nDrawing keymap..."
 keymap --config keymap_config.yaml draw --qmk-keyboard "crkbd/rev1" --layout-name "LAYOUT_split_3x6_3" --keys-only corne.yaml >corne.svg
--- images.sh
+++ images.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "Parsing ZMK keymap..."
-keymap -d -c draw/keymap_config.yaml parse -z config/corne.keymap -c 12 -l Base Diktor NAV FN NUM Sys Mouse >draw/corne.yaml
+keymap -d -c draw/keymap_config.yaml parse -z config/corne.keymap -c 12 -l Base Diktor NAV FN NUM SYM SYS Mouse >draw/corne.yaml
 echo -e "\nDrawing keymap..."
 keymap -c draw/keymap_config.yaml draw -k crkbd/rev1 -l LAYOUT_split_3x6_3 --keys-only draw/corne.yaml >draw/corne.svg
 keymap -c draw/keymap_config.yaml draw -k crkbd/rev1 -l LAYOUT_split_3x6_3 --select-layers Diktor --keys-only draw/corne.yaml >draw/corne_rus.svg
